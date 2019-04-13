2bfcf2266032c62bb939c37bd14227e7ddc706b5
Dominika Myszczynska <myszczynskad@gmail.com>
2019-04-13 01:00:35 +0100

    WDP190405-18:  dodalam karuzele

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,35 @@
-console.log('hello');
+import { tns } from '../../node_modules/tiny-slider/src/tiny-slider';
+
+const updateNav = info => {
+  for (let i = 0; i < info.navItems.length; i++) {
+    info.navItems[i].classList.toggle('tns-nav-hidden', i >= info.pages);
+  }
+};
+
+var slider = tns({
+  container: '#new-furnitures-products',
+  items: 1,
+  center: true,
+  slideBy: 'page',
+  controls: false,
+  navContainer: '#new-furnitures-dots',
+  speed: 400,
+  loop: false,
+  mouseDrag: true,
+  onInit: updateNav,
+  responsive: {
+    640: {
+      center: false,
+      items: 2
+    },
+    900: {
+      items: 3
+    },
+    1200: {
+      items: 4
+    }
+  }
+});
+
+slider.events.on('newBreakpointStart', updateNav);
+slider.events.on('newBreakpointEnd', updateNav);
